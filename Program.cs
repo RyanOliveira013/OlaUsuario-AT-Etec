f0380bbe83beb0ea0fc5d6ce75d6549219ede623
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-18 21:44:20 -0300

    Apenas o Começo de uma programaçao

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OlaUsuario_AT_Etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         string nomeDoUsuario;
+
+            Console.Write("Por favor, Digita Seu Nome: ");
+            nomeDoUsuario = Console.ReadLine();
+            Console.Write("Ola ");
+            Console.Write(nomeDoUsuario );
+            Console.WriteLine("Clique,em uma tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
